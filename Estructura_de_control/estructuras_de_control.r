cdc156e38726638d8e8d76fdc13c1d53f7f1b54d
said-tj <s.tecpa.j@gmail.com>
2023-08-02 12:50:19 -0600

    Estructuras de control

---
+++ Estructura_de_control/estructuras_de_control.r
@@ -0,0 +1,22 @@
+# if-else
+year <- 23 
+if(year > 18){
+  print("Persona adulta")
+}else{
+  print("Persona no adulta")
+}
+
+
+# for
+myvector <- c(1, 2, 3, 4, 5, 6)
+for (valor in myvector) {
+  print(valor)
+}
+
+
+# while
+r <- 10
+while(r < 60){
+  r <- r+1 
+}
+r
